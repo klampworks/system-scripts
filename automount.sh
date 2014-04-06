33bbc77039a0a8c6070870c216495119173992d0
yukipyon <wolfvbear@gmail.com>
2014-04-06 14:19:05 +0100

    Automount encrypted drives.

---
+++ automount.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#Spin in a loop looking for a specific media 
+#device, then decrypt and mount it.
+
+uuid="00000000-0000-0000-0000-000000000000"
+
+#Root please.
+sudo -v
+
+while :
+do
+	#Figure out which /dev/ device the desired media is.
+	enc_root=$(sudo blkid | sed -n "/$uuid/s/\(\/dev\/sd[a-z][0-9]\).*/\1/p")
+	
+	if [ -z $enc_root ]
+	then
+
+		#Keep trying until we see it.
+		echo "Could not detect harddrive, retying..."
+		sleep 1
+		continue
+
+	fi
+
+	#Decrypt and mount.
+	sudo cryptsetup luksOpen $enc_root 3t
+	sudo mount /dev/mapper/3t /mnt
+	exit
+done
