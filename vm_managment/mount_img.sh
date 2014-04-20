5d362dd50d24aedc82652b3d70d271851382d8fb
yukipyon <wolfvbear@gmail.com>
2014-04-20 12:29:29 +0100

    Added offsets.

--- vm_managment/mount_img.sh
+++ vm_managment/mount_img.sh
@@ -8,7 +8,14 @@ function setup {
 	mnt_point="/media/$(basename $1)"
 
 	#This is taken from the output of fdisk in the img file in sectors.
-	offset=2048
+	if [ -z "$2" ]
+	then
+		echo "No offset given, using default 2048."
+		offset=2048
+	else
+		echo "Using offset $2."
+		offset=$2
+	fi
 }
 
 if [ -z "$2" ]
@@ -20,7 +27,7 @@ fi
 if [ "$1" == "mount" ]
 then
 	echo "mounting..."
-	setup $2
+	setup $2 $3
 
 	sudo mkdir -p $mnt_point
 	sudo mount -o loop,offset=$(($offset * 512)) $2 $mnt_point
@@ -31,7 +38,7 @@ then
 	setup $2
 	
 	sudo umount $mnt_point 
-	rmdir $mnt_point
+	sudo rmdir $mnt_point
 else
 	echo "Unknown option, specify either mount or umount."
 fi
